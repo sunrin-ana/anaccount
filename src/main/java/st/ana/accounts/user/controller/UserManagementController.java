bc94911a148726c28bda85f73bff9931b9ac4b58
eunsoop <193854505+eunsoop@users.noreply.github.com>
2026-03-25 18:01:39 -0400

    feat: add user info endpoint

--- src/main/java/st/ana/accounts/user/controller/UserManagementController.java
+++ src/main/java/st/ana/accounts/user/controller/UserManagementController.java
@@ -1,4 +1,18 @@
 package st.ana.accounts.user.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserManagementController {
+    @GetMapping("/me")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) {
+        return principal.getAttributes();
+    }
 }
