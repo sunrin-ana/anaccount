fd2147af811322714f0538c50adb5f10183725b0
eunsoop <193854505+eunsoop@users.noreply.github.com>
2026-03-25 20:42:34 -0400

    fix: user info endpoint

--- src/main/java/st/ana/accounts/user/controller/UserManagementController.java
+++ src/main/java/st/ana/accounts/user/controller/UserManagementController.java
@@ -1,18 +1,24 @@
 package st.ana.accounts.user.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserManagementController {
     @GetMapping("/me")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) {
-        return principal.getAttributes();
+    public Map<String, Object> user(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("user_info", jwt.getClaims());
+        response.put("scopes", jwt.getClaim("scope"));
+
+        return response;
     }
 }
